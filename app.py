607e05db89cdbb83d39d339430db266a7950abbf
Jose Vilca Campana @marcusmors <alivezeh@gmail.com>
2024-06-20 17:35:00 -0500

    🚧 Creating paths with parameters and their templates, also i put the partials parts of a template in partials

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify, render_template
 from neo4j import GraphDatabase
 
 app = Flask(__name__)
@@ -9,35 +9,49 @@ user = "neo4j"
 password = "tu_contraseña"
 
 # Clase de utilidad para manejar la sesión de Neo4j
-class Neo4jSession:
-    def __init__(self, uri, user, password):
-        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+# class Neo4jSession:
+#     def __init__(self, uri, user, password):
+#         self._driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def close(self):
-        self._driver.close()
+#     def close(self):
+#         self._driver.close()
 
-    def run_query(self, query):
-        with self._driver.session() as session:
-            result = session.run(query)
-            return [record for record in result]
+#     def run_query(self, query):
+#         with self._driver.session() as session:
+#             result = session.run(query)
+#             return [record for record in result]
 
-neo4j_session = Neo4jSession(uri, user, password)
+
+@app.route('/user/<user_name>')
+def show_user_profile(user_name):
+    # get user_data
+    user = {"username": user_name}
+    
+    return render_template('userpage.html',user=user)
+
+@app.route('/user/<song_name>')
+def show_user_profile(song_name):
+    user = {"username": song_name}
+    
+    return render_template('songpage.html',user=user)
+
 
 # Rutas para cargar dinámicamente el contenido desde Neo4j
 @app.route('/featured')
 def load_featured():
     # Ejemplo de consulta a Neo4j
     query = "MATCH (s:Song) RETURN s.name LIMIT 5"
-    result = neo4j_session.run_query(query)
-    featured_songs = [record['s.name'] for record in result]
+    # result = neo4j_session.run_query(query)
+    # featured_songs = [record['s.name'] for record in result]
+    featured_songs = ["song_1", "song_2", "song_3"]
     return jsonify(items=featured_songs)
 
 @app.route('/myplaylists')
 def load_playlists():
     # Ejemplo de consulta a Neo4j
     query = "MATCH (p:Playlist) RETURN p.name LIMIT 5"
-    result = neo4j_session.run_query(query)
-    user_playlists = [record['p.name'] for record in result]
+    # result = neo4j_session.run_query(query)
+    # user_playlists = [record['p.name'] for record in result]
     return jsonify(items=user_playlists)
 
 @app.route('/')
@@ -49,4 +63,5 @@ def biblioteca():
     return render_template('biblioteca.html')
 
 if __name__ == '__main__':
+    # neo4j_session = Neo4jSession(uri, user, password)
     app.run(debug=True)
---
+++ classes/User.py
@@ -0,0 +1,11 @@
+""" 
+user_data
+username
+real_name
+address
+password=username
+
+"""
+
+# class User:
+#   __init__(self, ):
